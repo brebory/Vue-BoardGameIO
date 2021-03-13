128633deecd3c35274ebc015f523abcee3d06e00
Brendon Roberto <orph3us01@gmail.com>
2021-03-13 00:26:59 -0500

    feature: add boardgame.io component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <game></game>
 </template>
 
 <script>
+import Game from "./components/Game";
 export default {
-  name: "App",
+    components: {
+        game: Game,
+    },
+    name: "App",
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="game"></div>
+    <button @click="startClient">Start Client</button>
+</template>
+
+<script>
+import { Client } from "boardgame.io/client";
+import { TicTacToe } from "../game/TicTacToe";
+
+export default {
+    data() {
+        return {
+            client: null,
+        };
+    },
+
+    methods: {
+        startClient() {
+            console.log(`[Game.vue] - startClient`);
+            this.client = Client({ game: TicTacToe });
+            this.client.start();
+        },
+    },
+};
+</script>
